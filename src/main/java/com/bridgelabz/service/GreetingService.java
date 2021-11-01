b7463e1abb83eebed47c30618ef2236421550d24
padmini v <padmini.v@perfios.com>
2021-11-02 00:46:45 +0530

    MODIFIED: Ability for greeting app to find greeting message by id

--- src/main/java/com/bridgelabz/service/GreetingService.java
+++ src/main/java/com/bridgelabz/service/GreetingService.java
@@ -28,8 +28,7 @@ public class GreetingService implements IGreetingService
 	@Override
 	public Greeting getGreetingById(long id) 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return greetingRepository.getById(id);
 	}
 
 }
