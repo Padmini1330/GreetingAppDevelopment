23fc5e3824300cd8a1af7938956a95259ddbacdb
padmini v <padmini.v@perfios.com>
2021-11-02 01:12:00 +0530

    ADDED: deleteGreeting method signature in interface

--- src/main/java/com/bridgelabz/service/IGreetingService.java
+++ src/main/java/com/bridgelabz/service/IGreetingService.java
@@ -2,6 +2,8 @@ package com.bridgelabz.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.model.Greeting;
@@ -14,4 +16,5 @@ public interface IGreetingService
 	Greeting getGreetingById(long id);
 	List<Greeting> getAllGreetings();
 	Greeting editGreeting(long id,User user);
+	ResponseEntity<HttpStatus> deleteGreeting(long id);
 }	
